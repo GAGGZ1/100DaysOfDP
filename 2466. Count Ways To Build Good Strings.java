389a4c57477aa977a55ec56116ae0058b92019e3
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2025-01-03 00:27:23 +0530

    Update 2466. Count Ways To Build Good Strings.java

--- 2466. Count Ways To Build Good Strings.java
+++ 2466. Count Ways To Build Good Strings.java
@@ -1,28 +1,15 @@
 class Solution {
-    private static final int MOD = 1000000007;
-    private int[] dp;
-
-    private int countWays(int low, int high, int zero, int one, int pos) {
-        if (pos > high) {
-            return 0;
-        }
-        if (dp[pos] != -1) {
-            return dp[pos];
-        }
-
-        int count = 0;
-        if (pos >= low) {
-            count = 1;
-        }
-        count = (count + countWays(low, high, zero, one, pos + zero)) % MOD;
-        count = (count + countWays(low, high, zero, one, pos + one)) % MOD;
-
-        return dp[pos] = count;
-    }
-
     public int countGoodStrings(int low, int high, int zero, int one) {
-        dp = new int[high + 1];
-        Arrays.fill(dp, -1);
-        return countWays(low, high, zero, one, 0);
+        int maxLen = high + Math.max(zero,one);
+        int dp[] = new int[maxLen+1];
+        for(int len=high;len>=0;len--){
+            int zeroLen = len + zero;
+            int oneLen = len + one;
+            int zeroCount = (zeroLen >= low && zeroLen <= high)?1:0;
+            int oneCount = (oneLen >= low && oneLen <= high)?1:0;
+            int res = zeroCount + dp[zeroLen] + oneCount + dp[oneLen];
+            dp[len] = res % (1000000007);
+        }
+        return dp[0]; 
     }
 }
