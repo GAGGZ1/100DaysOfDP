b933a20cb66b71a367bb8a667b14511d7606aa7f
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2025-01-16 23:10:49 +0530

    Day 21 : 1277. Count Square Submatrices with All Ones.java

--- 1277. Count Square Submatrices with All Ones.java
+++ 1277. Count Square Submatrices with All Ones.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 if (matrix[i][j] == 1)
-                    dp[i + 1][j + 1] = 1 + Math.min(dp[i][j], Math.min(dp[i + 1][j], dp[i][j + 1]));
+                    dp[i + 1][j + 1] = 1 + Math.min(dp[i][j], Math.min(dp[i + 1][j], dp[i ][j + 1]));
                 ans += dp[i + 1][j + 1];
             }
         }
